fcac1ae62e73347616896920c43486c4cb7a66f8
carlos monroy <basesdedatosucc1920@gmail.com>
2021-09-02 22:22:45 -0500

    01. mi primer git para el programa

---
+++ my_proyect.cpp
@@ -0,0 +1,16 @@
+#include <studio.h>
+#include <math.h>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+        int ivalor =0;
+        int icontador=0;
+        int i =0;
+
+        cout <<"Ingrese un valor para calcular una suma.\n";
+        cin >> ivalor;
+
+        
